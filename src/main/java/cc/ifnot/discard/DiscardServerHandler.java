68eee45d9796cf825353f1cd9eb986946f26868b
hello <hello@ifnot.cc>
2018-01-25 18:18:14 +0800

    println

--- src/main/java/cc/ifnot/discard/DiscardServerHandler.java
+++ src/main/java/cc/ifnot/discard/DiscardServerHandler.java
@@ -3,13 +3,23 @@ package cc.ifnot.discard;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
 
 public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
 //        super.channelRead(ctx, msg);
-        ((ByteBuf) msg).release();
+        ByteBuf in = (ByteBuf) msg;
+
+        try {
+            while (in.isReadable()) {
+                System.out.println((char) in.readByte());
+                System.out.flush();
+            }
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
     }
 
     @Override
