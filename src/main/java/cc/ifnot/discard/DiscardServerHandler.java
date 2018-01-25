d7ccab3c5b88c27afbaee362311edf8cc5026a03
hello <hello@ifnot.cc>
2018-01-25 18:09:28 +0800

    add discard server handler

---
+++ src/main/java/cc/ifnot/discard/DiscardServerHandler.java
@@ -0,0 +1,21 @@
+package cc.ifnot.discard;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+//        super.channelRead(ctx, msg);
+        ((ByteBuf) msg).release();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+//        super.exceptionCaught(ctx, cause);
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
