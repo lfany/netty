a988cf85af0ca15b4f88701c61f70f4ff553e9da
hello <hello@ifnot.cc>
2018-01-29 14:25:40 +0800

    fix2 more

--- src/main/java/cc/ifnot/time/TimeDecoder.java
+++ src/main/java/cc/ifnot/time/TimeDecoder.java
@@ -2,17 +2,25 @@ package cc.ifnot.time;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.handler.codec.ReplayingDecoder;
 
 import java.util.List;
 
-public class TimeDecoder extends ByteToMessageDecoder {
+//public class TimeDecoder extends ByteToMessageDecoder {
+//    @Override
+//    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) {
+//        if (in.readableBytes() < 4) {
+//            return;
+//        }
+//
+//        out.add(in.readBytes(4));
+//    }
+//}
+
+public class TimeDecoder extends ReplayingDecoder<Void> {
+
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) {
-        if (in.readableBytes() < 4) {
-            return;
-        }
-
         out.add(in.readBytes(4));
     }
 }
